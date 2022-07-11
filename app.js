5f39b691aa59f0e37b05a10af6b9344d169cee79
keyur02 <keyur02>
2022-07-12 00:24:43 +0530

    show image in browser url

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use(express.static('public'));
-app.use('/public', express.static('images'));
+// app.use('/public', express.static('images'));
 
 app.get('/',async (req, res)=>{
 	res.send("ride share applicaton");
